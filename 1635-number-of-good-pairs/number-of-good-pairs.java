17a9667af3cc1064a51e2811d7c1c0eba1b709b3
shrashti-pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-02-01 13:40:28 +0530

    Time: 1 ms (86.61%) | Memory: 41.6 MB (5.52%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -3,10 +3,7 @@ class Solution {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if (i == j) {
-                    break;
-                }
-                if (nums[i] == nums[j]) {
+                if (i < j && nums[i] == nums[j]) {
                     result++;
                 }
             }
