d5126da55d0ae09a4300bb7d39ea81941e357083
shrashti-pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-02-03 20:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (23.02%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -6,6 +6,9 @@ class Solution {
 
         while (start < end) {
             int mid = start + (end - start) / 2;
+            //checking middle element is odd or even because it follows the pattern
+            //[0,1] [2,3] [4,5] then number is at left side 
+            //[3,4] [5,6] [7,8] then number is at right side 
             if (mid % 2 == 0) {
                 mid++;
             }
