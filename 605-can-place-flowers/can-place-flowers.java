ac671adfb7a7e17e96d5857c7e98f96b1acffa75
shrashti-pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-02-13 18:10:49 +0530

    Time: 1 ms (97.28%) | Memory: 45.4 MB (64.87%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,35 +1,22 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        
+        if (n == 0)
+            return true;
 
-       for(int i= 0; i< flowerbed.length; i++){
-           if(n == 0){
-               return true;
-           }
-           if(flowerbed.length == 1){
-               if(flowerbed[i] == 0){
-                   n--;
-               }
-           }
-           if(flowerbed.length > 1 &&flowerbed[i] == 0){
-               if(i == 0 && flowerbed[i+1] == 0){
-                   flowerbed[i] = 1;
-                   n--;
-               }
-               else if( i > 0 && i < flowerbed.length -1 && 
-               flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
-                   flowerbed[i] = 1;
-                   n--;  
-               }else if(i == flowerbed.length - 1 && flowerbed[i-1] == 0){
-                   flowerbed[i] = 1;
-                   n--;
-               }
-           }
-           if(n == 0){
-               return true;
-           }
-       }
-       return false;
-       
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.length - 1 || flowerbed[i + 1] == 0 )) {
+                flowerbed[i] = 1;
+                count++;
+            }
+
+            if (count == n) {
+                return true;
+            }
+        }
+        return false;
+
     }
 }
