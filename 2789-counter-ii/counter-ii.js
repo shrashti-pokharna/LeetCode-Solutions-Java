6987583d8d282837487e05322133397cca3f92c1
Shrashti Pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-07-07 12:24:20 +0530

    Time: 60 ms (53.54%) | Memory: 51.7 MB (54.12%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var counter = init;
+    const increment = () => {
+        return ++counter;
+    }
+    const decrement = () => {
+        return --counter;
+    }
+    const reset  = () => {
+        counter = init
+        return counter;
+    }
+    return {increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
