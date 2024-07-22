f7bc6909333984a8a41892d5503bdcab9587c0cc
Shrashti Pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-07-22 10:58:58 +0530

    Time: 69 ms (91.13%) | Memory: 62.4 MB (47.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,15 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
+var containsDuplicate = function (nums) {
     let set = new Set()
-    for(const num of nums){
-        if(set.has(num)){
-            return true;
-        }else{
-            set.add(num)
-        }
+    for (const num of nums) {
+        if (set.has(num)) return true;
+        set.add(num)
     }
     return false;
-    
+
 };
