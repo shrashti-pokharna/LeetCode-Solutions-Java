ada619333a365757cf6217aa88506cf090f98038
Shrashti Pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-07-23 12:47:30 +0530

    Time: 57 ms (35.89%) | Memory: 49.1 MB (26.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -11,9 +11,8 @@ var search = function (nums, target) {
         let mid = Math.floor((left + right) / 2);
         if (nums[mid] === target) {
             return mid;
-        }
-         
-        if (nums[left] <= nums[mid]){
+        } 
+        else if (nums[left] <= nums[mid]){
             if(nums[left] <= target && target < nums[mid]){
                 right = mid - 1;
             }else{
