4b2ef363d5661526f840c843bbcf52c41d828690
Shrashti Pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2024-07-25 12:19:55 +0530

    Time: 60 ms (21.10%) | Memory: 48.9 MB (41.69%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -2,18 +2,15 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps = function(num) {
-    return helper(num, 0)    
-};
-
-var helper = (num, count) => {
+var numberOfSteps = function(num, count = 0) {
     if(num === 0){
         return count;
     }
     if(num % 2 === 0){
-        return helper(num/2, count+1)
+        return numberOfSteps(num/2, count+1)
     }else{
-        return helper(num - 1, count+1)
+        return numberOfSteps(num - 1, count+1)
     }
 
-}
+};
+
